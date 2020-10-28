89e116d207d5ac6fe3137efe80aae9ef7e860faa
Daniele Cervi <daniele.cervi@4ward.it>
2020-10-28 22:34:21 +0100

    Moved to tailwind css, recreated header bar

---
+++ Client/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'black-pearl': {
+          50: '#F2F3F4',
+          100: '#E6E8E9',
+          200: '#C0C4C8',
+          300: '#9AA1A7',
+          400: '#4F5B66',
+          500: '#031424',
+          600: '#031220',
+          700: '#020C16',
+          800: '#010910',
+          900: '#01060B',
+        },
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
